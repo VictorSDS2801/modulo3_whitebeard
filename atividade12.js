6093bc902377e2b46553f319e0a6e7f5d858b1c5
Victor <victordiogo2801@gmail.com>
2025-12-23 12:01:13 -0300

    atividade 12 modulo 3

--- atividade12.js
+++ atividade12.js
@@ -4,25 +4,28 @@ const rl = readline.createInterface({
     input: process.stdin, 
     output: process.stdout
 })
+
 let usuario = {
     nome: "Victor",
     idade: 14, 
     curso: "Engenharia de software",
     media: 9.7
 }
-function programa() {
-    console.log("Digite alguma chave (nome, idade, curso ou media) para verificar se existe no objeto. ")
-    rl.question("Digite a sua resposta: ", resposta)
-}
-function resposta() {
-    let chavebuscada = resposta.trim()
 
-    if (chavebuscada in usuario) {
-        console.log("A chave que você buscou existe no objeto do usuário!")
-    }
-    else {
-        console.log("A chave buscada não existe no objeto do usuário!")
-    }
-    rl.close()
+function programa() {
+    console.log("Digite alguma chave (nome, idade, curso ou media) para verificar se existe no objeto.")
+    
+    rl.question("Digite a sua resposta: ", (resposta) => {
+        let chavebuscada = resposta.trim()
+
+        if (chavebuscada in usuario) {
+            console.log("A chave que você buscou existe no objeto do usuário!")
+        } else {
+            console.log("A chave buscada não existe no objeto do usuário!")
+        }
+
+        rl.close()
+    })
 }
+
 programa()
